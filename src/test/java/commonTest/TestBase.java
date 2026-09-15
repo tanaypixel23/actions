82b8b57b52e194ce8b4915204285248695e2d713
Tanay Pande <318622588+tanaypixel23@users.noreply.github.com>
2026-09-15 12:53:50 +0530

    haedless mode

--- src/test/java/commonTest/TestBase.java
+++ src/test/java/commonTest/TestBase.java
@@ -8,34 +8,77 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 
 public class TestBase {
 
     protected WebDriver driver;
 
-    @Parameters("browser")
     @BeforeMethod
+    @Parameters("browser")
     public void setupDriver(@Optional("chrome") String browser) {
 
+        boolean isGitHubActions = System.getenv("GITHUB_ACTIONS") != null;
+
         switch (browser.toLowerCase()) {
+
             case "firefox":
-                DriverFactory.setDriver(new FirefoxDriver());
+
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+
+                if (isGitHubActions) {
+                    firefoxOptions.addArguments("--headless");
+                    firefoxOptions.addArguments("--width=1920");
+                    firefoxOptions.addArguments("--height=1080");
+                }
+
+                DriverFactory.setDriver(new FirefoxDriver(firefoxOptions));
                 break;
 
             case "edge":
-                DriverFactory.setDriver(new EdgeDriver());
+
+                EdgeOptions edgeOptions = new EdgeOptions();
+
+                if (isGitHubActions) {
+                    edgeOptions.addArguments("--headless=new");
+                    edgeOptions.addArguments("--no-sandbox");
+                    edgeOptions.addArguments("--disable-dev-shm-usage");
+                    edgeOptions.addArguments("--disable-gpu");
+                    edgeOptions.addArguments("--window-size=1920,1080");
+                }
+
+                DriverFactory.setDriver(new EdgeDriver(edgeOptions));
                 break;
 
+            case "chrome":
             default:
-                DriverFactory.setDriver(new ChromeDriver());
+
+                ChromeOptions chromeOptions = new ChromeOptions();
+
+                if (isGitHubActions) {
+                    chromeOptions.addArguments("--headless=new");
+                    chromeOptions.addArguments("--no-sandbox");
+                    chromeOptions.addArguments("--disable-dev-shm-usage");
+                    chromeOptions.addArguments("--disable-gpu");
+                    chromeOptions.addArguments("--window-size=1920,1080");
+                }
+
+                DriverFactory.setDriver(new ChromeDriver(chromeOptions));
+                break;
         }
 
         driver = DriverFactory.getDriver();
 
-        driver.manage().window().maximize();
-        driver.get(
-                "https://surajkumar-ibm.github.io/Selenium-Miniproject-Application/"
-        );
+        if (!isGitHubActions) {
+            driver.manage().window().maximize();
+        }
+
+        driver.get("https://surajkumar-ibm.github.io/Selenium-Miniproject-Application/");
     }
 
     @AfterMethod
