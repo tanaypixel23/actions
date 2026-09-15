5b3d8a628206e79a2750641916104ac46c95908c
Tanay Pande <318622588+tanaypixel23@users.noreply.github.com>
2026-09-15 14:41:16 +0530

    tset no 596

--- src/test/java/Trello_TestScript/TC01_Product.java
+++ src/test/java/Trello_TestScript/TC01_Product.java
@@ -52,10 +52,11 @@ public class TC01_Product extends TestBase {
         SearchProduct("Mascara");
     }
 
-    @Test
+    /*@Test
     public void dummy() {
         Assert.fail();
     }
+    */
 }
 
 
