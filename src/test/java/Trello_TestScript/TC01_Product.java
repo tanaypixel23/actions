abcbaab3c81d5b86b8cb07a2acd5c0001e76f5dc
Tanay Pande <318622588+tanaypixel23@users.noreply.github.com>
2026-09-15 14:37:08 +0530

    Fix allure result path

--- src/test/java/Trello_TestScript/TC01_Product.java
+++ src/test/java/Trello_TestScript/TC01_Product.java
@@ -3,18 +3,25 @@ package Trello_TestScript;
 import commonTest.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 
 public class TC01_Product extends TestBase {
 
     public void SearchProduct(String productName) {
 
-        driver.findElement(By.linkText("Products")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-        WebElement searchBox = driver.findElement(By.xpath("//input[@id='searchinput']"));
+        wait.until(ExpectedConditions.elementToBeClickable(By.linkText("Products"))).click();
+
+        WebElement searchBox = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("searchinput")));
+
+        searchBox.clear();
         searchBox.sendKeys(productName);
         boolean found = false;
 
